252369504f0d11ac8224abafbc605e2c495f02b5
mac <mac@macui-MacBook-Air.local>
2022-12-05 15:15:27 +0900

    test

---
+++ src/main/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./index.css";
+import axios from "axios";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get("https://576c94d0-354a-4684-b794-5981e1b1f4e7.mock.pstmn.io/product")
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 :", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+        <h1>판매 상품</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                  <div className="product-contents">
+                    <div>
+                      <span className="product-name">{product.name}</span>
+                    </div>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPage;
