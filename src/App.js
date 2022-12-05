2131e401a59cfcda8f49cf262d9d8342551aef40
mac <mac@macui-MacBook-Air.local>
2022-12-05 16:25:38 +0900

    Link tag update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import MainPage from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import ProductPage from "./products/index.js";
+import UploadPage from "./upload";
 
 function App() {
   return (
     <div>
-      <MainPage></MainPage>
+      <div>
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPage />
+          </Route>
+          <Route exact={true} path={"/products/:id"}>
+            <ProductPage />
+          </Route>
+          <Route exact={true} path={"/upload"}>
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
